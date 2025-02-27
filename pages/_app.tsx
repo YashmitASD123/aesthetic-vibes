069c77e2694c1c4254adbf0459aef68d4db44c4d
Shridhar Ambernath Sagari <18202@bandhanlife.com>
2025-02-27 22:14:54 +0530

    Use Bun &

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import '../styles/tailwind.css';
 import Layout from '../components/Layout';
 import type { AppProps } from 'next/app';
@@ -12,14 +11,3 @@ function MyApp({ Component, pageProps }:AppProps) {
 }
 
 export default MyApp;
-=======
-import { AppProps } from "next/app";
-
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Component {...pageProps} />
-  );
-};
-
-export default App;
->>>>>>> 684b5b80decf600b82e91502a887f622f6a58f13
